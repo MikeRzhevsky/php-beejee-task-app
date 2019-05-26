45a2b1e3704c3832ac9e51424b566a599e61e106
@MikeRzhevsky <39048059+MikeRzhevsky@users.noreply.github.com>
2019-05-26 21:04:37 +0300

    Add files via upload

--- Views/View.php
+++ Views/View.php
@@ -1,1 +1,26 @@
+<?php
+namespace  Views;
 
+class View
+{
+
+    protected $data=[];
+
+
+    public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function __set($name, $value)
+    {
+        $this->data[$name]=$value;
+    }
+
+    public function display($tempalte)
+    {
+        include $tempalte;
+    }
+
+}
+?>
