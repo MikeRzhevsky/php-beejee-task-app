4d30f7a135984e0c377d3e05c2fc440ec23dca82
@MikeRzhevsky <39048059+MikeRzhevsky@users.noreply.github.com>
2019-05-26 21:07:07 +0300

    Add files via upload

--- Views/Layouts/footer.php
+++ Views/Layouts/footer.php
@@ -1,1 +1,7 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white" >Copyright © Mike Rzhevsky 2018</p>
+    </div>
+</footer>
 
+</html>
---
+++ Views/Layouts/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta  charset=UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>приложение-задачник</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../Templates/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<nav class="navbar navbar-default">
+
+    <div class="container-fluid">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/tasks/">Tasks app</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="/tasks">Tasks <span class="sr-only">(current)</span></a></li>
+                <li><a href="/task/add">Add task</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+
+               <?php if(!empty($_SESSION) && $_SESSION['admin']):
+                   echo "<li><a href='/logout/'>Logout</a></li>";
+                 else:
+                   echo "<li><a href='/login/'>Login</a></li>";
+                 endif;
+               ?>
+            </ul>
+        </div> <!-- /.navbar-collapse -->
+
+    </div> <!-- /.container-fluid -->
+</nav>
